25255035a152c88ae8aafb6fd24b1f7973c0296c
crypto3ye <crypto3ye@gmail.com>
2022-03-30 01:07:52 +0400

    Counter contract

---
+++ yt-soldtyprgrmr/Counter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
+
+contract Counter{
+    uint public count;
+
+    function inc() external {
+        count = count +1;
+    }
+
+    function dec() external {
+        count = count -1;
+    }
+}
