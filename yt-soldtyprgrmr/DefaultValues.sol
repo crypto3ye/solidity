0b7a9c8ae4a2202795d055d5680c375cc7eed212
crypto3ye <crypto3ye@gmail.com>
2022-03-31 02:09:26 +0400

    Added default values

---
+++ yt-soldtyprgrmr/DefaultValues.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract DefaultValues {
+    bool public b; // Default value - flase
+    uint public u; // 0
+    int public i; // 0
+    address public a; //0x0000000000000000000000000000000000000000 (40 Zeros)
+     
+}
+
